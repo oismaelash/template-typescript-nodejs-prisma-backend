b1fa6c6ec89b628b136d98a701b0a791f914591b
ismaelash <contato@ismaelnascimento.com>
2022-08-23 21:21:01 -0300

    [DELETED] Migrations and User model

--- src/controllers/UsersController.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '@models/User'
-
-export async function getOneUser (id: string): Promise<User> {
-  const user = new User(id, 'Ismael Ash', 'contato@ismaelnascimento.com')
-  return user
-}
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express'
-import * as UsersController from '@controllers/UsersController'
 import * as PostController from '@controllers/PostController'
 
 const app = express()
 
 app.get('/', (request, response) => {
   return response.json({ message: 'backend works!' })
-})
-
-app.get('/users/:id', async (request, response) => {
-  console.log('userId', request.params.id)
-  const user = await UsersController.getOneUser(request.params.id)
-  return response.json(user)
 })
 
 app.get('/posts', async (request, response) => {
